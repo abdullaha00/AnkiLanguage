11e337e0b12c34dd49ce0d46a0370e6bfab91b47
Abdullah Akram <abdullaha3474@gmail.com>
2025-09-28 01:46:30 +0100

    Implement sidebar and navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
-import SideNav from "./sidenav";
+import SideNav from "../components/AppSidebar";
 
 export default function Home() {
 
   return (
-    <div className="flex">
-
+    <div className="flex justify-center items-center">
+        <span>Home</span>
   </div>
   );
 }
---
+++ components/AppSidebar.tsx
@@ -0,0 +1,105 @@
+
+"use client"
+import { Calendar, Home, Inbox, Search, Settings, Languages, User2, ChevronUp } from "lucide-react";
+import { Sidebar, SidebarContent, SidebarFooter, SidebarGroupLabel, SidebarHeader, SidebarGroup, SidebarGroupContent, SidebarMenuItem, SidebarMenu, SidebarMenuButton, SidebarSeparator } from "./ui/sidebar";
+import Link from "next/link";
+import Image from "next/image";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "./ui/dropdown-menu";
+import { TbLanguageKatakana } from "react-icons/tb";
+import { GrScan } from "react-icons/gr";
+import { GiCardPick } from "react-icons/gi";
+
+// Menu items.
+const items = [
+    {
+      title: "Home",
+      url: "/",
+      icon: Home,
+    },
+    {
+      title: "Anki",
+      url: "/anki",
+      icon: GiCardPick,
+    },
+    {
+      title: "OCR",
+      url: "/ocr",
+      icon: GrScan,
+    }
+  ];
+
+
+export function AppSidebar() {
+
+    return (
+        
+        <Sidebar collapsible="icon">
+        <SidebarHeader>
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <SidebarMenuButton asChild>
+                <Link href="/">
+                <TbLanguageKatakana />
+                <span>Japanese</span>
+                </Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          </SidebarMenu>
+
+
+        </SidebarHeader>
+        <SidebarSeparator/>
+            <SidebarContent>
+                <SidebarGroup>
+                    <SidebarGroupLabel>Application</SidebarGroupLabel>
+                    <SidebarGroupContent>
+                        <SidebarMenu>
+                            {items.map(item => <SidebarMenuItem key={item.title}>
+
+                                <SidebarMenuButton asChild>
+                                    <Link href={item.url}> 
+                                        <item.icon/>
+                                        <span>{item.title}</span>
+                                    </Link>
+                                </SidebarMenuButton>
+
+                            </SidebarMenuItem>)}
+
+                        </SidebarMenu>
+
+                    </SidebarGroupContent>
+                </SidebarGroup>
+            </SidebarContent>
+            <SidebarFooter>
+              <SidebarMenu>
+                <SidebarMenuItem>
+
+                  <DropdownMenu>
+
+                    <DropdownMenuTrigger asChild>
+                      <SidebarMenuButton>
+                        <User2/>Guest<ChevronUp className="ml-auto size-4" />
+                      </SidebarMenuButton>
+                    </DropdownMenuTrigger>
+
+                    <DropdownMenuContent align="end">
+
+                      <DropdownMenuItem>Account</DropdownMenuItem>
+                      <DropdownMenuItem>Settings</DropdownMenuItem>
+                    </DropdownMenuContent>
+
+                  </DropdownMenu>
+
+                </SidebarMenuItem>
+              </SidebarMenu>
+            </SidebarFooter>
+        
+        </Sidebar>
+
+    );
+    
+
+}
+
+export default AppSidebar;
+
